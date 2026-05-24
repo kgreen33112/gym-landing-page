281259fa738aa18932e8ee98b14dad4284ff2613
kgreen33112 <kgreencoprods@gmail.com>
2026-05-24 18:52:43 -0500

    Interaction additions

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
+const joinBtn = document.getElementById('joinBtn');
+const pricingSection = document.querySelector(".pricing-section");
 
 hamburger.addEventListener("click", () => {
     navList.classList.toggle("active");
+});
+
+joinBtn.addEventListener('click', ()  => {
+    pricingSection.scrollIntoView({ behavior: 'smooth', block: 'start'});
 });
